74fbb0d9f3b52a6fabc8078807d8a009fc6f3870
FrancoAguirre644 <francoaguirre644@gmail.com>
2022-07-01 16:07:24 -0300

    Add database config

--- api/server/index.ts
+++ api/server/index.ts
@@ -1,7 +1,8 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
+
+import db from './database/config';
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,16 @@ app.get('/', (req: Request, res: Response) => {
     res.json({ msg: 'Hello World!' });
 });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at https://localhost:${port}`);
-});
+(async () => {
+
+    try {
+        await db.authenticate();
+        await db.sync({ alter: true });
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+
+})();
+
+app.listen(port, () => console.log(`[server]: Server is running at https://localhost:${port}`));
